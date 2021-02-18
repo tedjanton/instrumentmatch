e0ef5c525f44ae4ec070a1727cc755d154e7d1aa
Ted Anton <tedjanton@gmail.com>
2021-02-18 11:59:33 -0600

    finished backend setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,5 @@ const router = express.Router();
 
 router.use("/api", apiRouter);
 
-router.get("/hello/world", (req, res) => {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World");
-})
 
 module.exports = router;
