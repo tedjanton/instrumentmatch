f2e8eb7f9f94ff65a89bdd01640dc5a704fe24ee
Ted Anton <tedjanton@gmail.com>
2021-02-18 17:43:24 -0600

    built sign up thunk action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,6 +32,18 @@ export const restoreUser = () => async dispatch => {
   return res;
 }
 
+export const signup = (user) => async dispatch => {
+  const { username, email, password } = user;
+  const res = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({ username, email, password })
+  })
+
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
+}
+
 const initialState = { user: null }
 
 const sessionReducer = (state = initialState, action) => {
