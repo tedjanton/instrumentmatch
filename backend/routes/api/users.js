59971d006e8a13f2f316fe08d2f57a8519bba104
Ted Anton <tedjanton@gmail.com>
2021-02-18 11:30:12 -0600

    built user signup route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
 const router = express.Router();
 
+router.post("/", asyncHandler(async (req, res) => {
+  const { email, password, username } = req.body;
+  const user = await User.signup({ email, username, password });
 
+  await setTokenCookie(res, user);
+
+  return res.json({ user });
+}));
 
 module.exports = router;
+
+
+
+// fetch('/api/users', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `P3IO0jvH-FF_Br2MhNOUHJHs3Z6mLo3r9c8Y`
+//   },
+//   body: JSON.stringify({
+//     email: 'spidey@spider.man',
+//     username: 'Spidey',
+//     password: 'password'
+//   })
+// }).then(res => res.json()).then(data => console.log(data));
